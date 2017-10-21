e58493ce5c464ab73a5b1350e11ad56dbf35ce38
Carole Aymon <carole.aymon@gmail.com>
2017-10-21 18:03:16 +0200

    added commentary

--- program.rb
+++ program.rb
@@ -5,3 +5,7 @@ def greeting
 end
 
 greeting
+
+#variable = let you  set  a  value to an element so the computer will remember it ,  for  example  name  =   Carole
+#method = a series of statements that performs an operation or a series of operations and can  be called many  times similar to  a  function.
+#"2" +"2" is no equal 4  because they are string not integrer
